2f20ecb607998b4153e8314c6ef97b3592983c54
imkuldeepahlawat <imkuldeepahlawat@hotmail.com>
2023-07-07 11:11:19 +0530

    --feat hero section skeleton completed

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./style.scss";
+import Herobanner from './heroBanner/Herobanner';
+
+const Home = () => {
+  return (
+    <div className='homePage'>
+        <Herobanner/>
+    </div>
+  )
+}
+
+export default Home
