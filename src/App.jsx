2633003e421b727d6036fbae2fe14d861ef64727
imkuldeepahlawat <imkuldeepahlawat@hotmail.com>
2023-07-07 08:49:10 +0530

    --redux store setup complete

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
+
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
+
+function App() {
+  const dispatch = useDispatch();
+  const url = useSelector((state) => state.home.url);
+  console.log(url)
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+      dispatch(getApiConfiguration(res));
+    });
+  };
+
+  return <div>{url?.total_pages}</div>;
+}
+
+export default App;
